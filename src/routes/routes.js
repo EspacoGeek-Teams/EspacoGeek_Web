652e81ab861ec311ce4f1e9abbdf4a346d090afc
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2025-10-07 20:26:34 -0300

    delete depreciated
    Deleted the RecoverPassword.jsx container and updated src/routes/routes.js to remove its references. Also added package-lock.json to .gitignore to prevent it from being tracked in version control.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import RecoverPassword from "./../containers/recoverPassword/RecoverPassword";
 import Home from "./../containers/home/Home";
 import Media from "./../containers/media/Media";
 
@@ -8,7 +7,6 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={<Home />} />
-            <Route path="recoverPassword" element={<RecoverPassword />} />
             <Route path="media/:mediaId/:mediaName" element={<Media />} />
         </>
     )
