9f8a522d3f49ce335751bb89e656af37387b6643
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-02-26 23:14:33 -0300

    Import initReactI18next from react-i18next
    Replace the previous subpath import from 'react-i18next/initReactI18next' with the named export from 'react-i18next'. This resolves module import issues and aligns with the current react-i18next package exports without changing runtime behavior.

--- src/i18n/config.jsx
+++ src/i18n/config.jsx
@@ -1,5 +1,5 @@
 import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next/initReactI18next';
+import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEN from './locales/en/translation.json';
 import translationPT from './locales/pt/translation.json';
